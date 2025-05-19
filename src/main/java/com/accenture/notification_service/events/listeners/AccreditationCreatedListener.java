2fff99ace3c2aea81815535239109b2212c52fac
Alejandra Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2025-05-19 10:17:41 -0300

    add AccreditationCreatedListener

---
+++ src/main/java/com/accenture/notification_service/events/listeners/AccreditationCreatedListener.java
@@ -0,0 +1,37 @@
+package com.accenture.notification_service.events.listeners;
+
+import com.accenture.notification_service.client.dtos.AccreditationDtoOutput;
+import com.accenture.notification_service.exceptions.EmailException;
+import com.accenture.notification_service.services.EmailService;
+import com.accenture.notification_service.services.PdfGeneratorService;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class AccreditationCreatedListener {
+    
+    private final EmailService emailService;
+    private final PdfGeneratorService pdfGeneratorService;
+
+    @RabbitListener(queues = "${rabbitmq.queue.accreditation}")
+    public void handleOrderCreatedEvent(AccreditationDtoOutput accreditationDtoOutput) {
+        log.info("Order received: {}", accreditationDtoOutput.getAccreditationId());
+        log.info("Searching email's user with ID: {}", accreditationDtoOutput.getUserId());
+
+        String userEmail = accreditationDtoOutput.getEmail();
+
+        if (userEmail == null || userEmail.isBlank()) {
+            log.error("User email is missing for accreditation ID: {}", accreditationDtoOutput.getAccreditationId());
+            throw new EmailException("Missing email for user ID: " + accreditationDtoOutput.getUserId());
+        }
+
+        byte[] pdfContent = pdfGeneratorService.generateAccreditationPdf(accreditationDtoOutput);
+        emailService.sendAccreditationEmail(userEmail, pdfContent);
+    }
+}
--- src/main/java/com/accenture/notification_service/exceptions/EmailException.java
+++ src/main/java/com/accenture/notification_service/exceptions/EmailException.java
@@ -8,4 +8,8 @@ public class EmailException extends BusinessException {
     public EmailException(String message, Throwable throwable) {
         super(message, throwable);
     }
+
+    public EmailException(String message) {
+        super(message);
+    }
 }
